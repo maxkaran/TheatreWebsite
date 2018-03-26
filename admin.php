a30632fe61a323f78296e919129d23827be7af21
Cooper Thomas-Clark <14ctc2@queensu.ca>
2018-03-26 11:36:20 -0400

    Final changes

--- admin.php
+++ admin.php
@@ -14,58 +14,23 @@
 	</div>
 	<div class="container">
 		<form method="post" action="search-customers.php">
-			<label for="customers" color="black">List Accounts</label> 
-			<button type="submit" class="btn" name="search" style="margin: auto; display: block-inline;">Go!</button>
+			<label for="customers" color="black">Search an Account  </label> 
+			<INPUT TYPE=TEXT NAME="customers"> 
+			<button type="submit" class="btn" name="search" style="margin: auto; display: block-inline;">Search!</button>
+			<label for="customers" style="font-size: 15px;" color="black">(leave empty for all accounts)</label> 
 		</form>
 		
 		<br />
 		
+<<<<<<< HEAD
+		<form method="post" action=
+=======
 		<form method="post" action="edit-theatres.php">
 			<label for="customers" color="black">Edit Theatre Complexes </label> 
 			<button type="submit" class="btn" name="search" style="margin: auto; display: block-inline;">Go!</button>
 		</form>
 		
-		<br />
-		
-		<form method="post" action="edit-movies.php">
-			<label for="customers" color="black">Edit Movies </label> 
-			<button type="submit" class="btn" name="search" style="margin: auto; display: block-inline;">Go!</button>
-		</form>
-		
-		<br />
-		<?php
-			$myPDO = new PDO('mysql:host=localhost;dbname=movietheatredatabase', 'root', '');
-			$query = $myPDO->prepare("SELECT SUM(number_tickets) AS tot_tickets, complex_name FROM reservation GROUP BY complex_name");
-			$query->execute();
-		
-	
-			$most_pop = $query->fetch(PDO::FETCH_ASSOC);
-			$most_pop_complex = $most_pop['complex_name'];
-			echo "<p style=\"color: black;\">The most popular theatre complex is $most_pop_complex!</p>";
-			
-			$query1 = "SELECT SUM(number_tickets) AS tot_tickets, complex_name, screen_number, start_time FROM reservation GROUP BY complex_name";
-			$query2 = "SELECT MAX(tot_tickets), complex_name, screen_number, start_time FROM ($query1)";
-			
-			$query = $myPDO->prepare($query2);
-			$query->execute();
-			
-			$most_pop = $query->fetch(PDO::FETCH_ASSOC);
-			
-			$complex_name = $most_pop['complex_name'];
-			$screen_number = $most_pop['screen_number'];
-			$start_time = $most_pop['start_time'];
-			
-			$query3 = "SELECT movie_title FROM showing where start_time=$start_time, complex_name=$complex_name, screen_number=$screen_number";
-			
-			$query = $myPDO->prepare($query3);
-			$query->execute();
-		
-	
-			$most_pop = $query->fetch(PDO::FETCH_ASSOC);
-			$most_pop_movie = $most_pop['movie_title'];
-			echo "<p style=\"color: black;\">The most popular movie is $most_pop_movie!</p>";
-		?>
-		
+>>>>>>> a458b7085d5a2c7818daa97cd9ad377d7b98476f
 	</div>
   </body>
 </html>
